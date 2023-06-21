ea3f837bc62a2c38b3702738007c452934c0ab18
RogerSi666 <annanecha1991@gmail.com>
2023-06-21 17:09:11 +0900

    shop model create

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -5,9 +5,12 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Owner;
+use App\Models\Shop;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Hash;
+use Throwable;
+use Illuminate\Support\Facades\Log;
 
 
 class OwnersController extends Controller
@@ -70,11 +73,27 @@ class OwnersController extends Controller
             'email' => 'required|string|email|max:255|unique:owners',
             'password' => 'required|string|confirmed|min:8',
         ]);
-        Owner::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+
+        try{
+            DB::transaction(function() use($request) {
+                $owner = Owner::create([
+                            'name' => $request->name,
+                            'email' => $request->email,
+                            'password' => Hash::make($request->password),
+                        ]);
+                Shop::create([
+                    'owner_id' => $owner->id,
+                    'name' => '店名を入力してください',
+                    'information' => '',
+                    'filename' => '',
+                    'is_selling' => true
+                ]);
+        }, 2);
+        }catch(Throwable $e){
+            Log::error($e);
+            throw $e;
+        }
+       
         return redirect()
         ->route('admin.owners.index')
         ->with(['message' => 'オーナー登録を実施しました。',
